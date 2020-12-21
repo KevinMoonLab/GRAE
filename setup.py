b1ceffc8197c2b50990d97eaeb34b6ae9254240d
morin <morin.sacha@gmail.com>
2020-12-21 15:19:29 -0500

    Specific versions for requirement

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup, find_packages
 
 setup(name='src',
       version='0.1',
+      python_requires='>=3.8',
       description='main package',
       packages=find_packages())
