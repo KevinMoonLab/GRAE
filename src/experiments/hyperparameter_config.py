afc3f33f4fac62346f860bbe02653a91a6423430
Sacha Morin <chu24@beluga1.int.ets1.calculquebec.ca>
2021-01-08 12:10:36 -0500

    New hyperparameter distributions.

--- src/experiments/hyperparameter_config.py
+++ src/experiments/hyperparameter_config.py
@@ -3,7 +3,7 @@ import scipy.stats as stats
 neighbor_param = [5, 10, 20]
 
 PARAM_GRID = {
-    'lr': stats.loguniform(a=1e-3, b=1e-2),
+    'lr': stats.loguniform(a=2e-4, b=2e-3),
     'batch_size': stats.randint(low=32, high=101),
     'weight_decay': [0],
     't': [10, 25, 50, 100, 250],
@@ -12,7 +12,7 @@ PARAM_GRID = {
     'n_neighbors': neighbor_param,
     'min_dist': stats.uniform(loc=0, scale=.99),
     'epsilon': stats.loguniform(a=.01, b=250),
-    'lam': stats.loguniform(a=1e-1, b=5),
+    'lam': stats.loguniform(a=1e-1, b=10),
     'margin': stats.loguniform(a=.01, b=100),
 }
 
